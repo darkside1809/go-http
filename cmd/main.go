1fa6f1150b17b9c78aa8003877a1682e47b7f09f
Nekruz <njpnek@gmail.com>
2021-05-21 20:50:55 +0500

    add methods: get, save, delete update for banners & optimize code

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,49 @@
 package main
 
 import (
-	"log"
+	//"log"
 	"net"
+	"net/http"
+	"os"
+	"sync"
 
-	"github.com/darkside1809/http/pkg/server"
+	"github.com/darkside1809/http/cmd/app"
+	"github.com/darkside1809/http/pkg/banners"
 )
 
+type handler struct {
+	mu 		*sync.RWMutex
+	handlers	 map[string]http.HandlerFunc
+}
 
 func main() {
-
 	host := "0.0.0.0"
 	port := "9999"
-	srv := server.NewServer(net.JoinHostPort(host,port))
 
-	err := srv.Start()
-	if err != nil {
-		log.Print(err)
+	if err := execute(host, port); err != nil {
+		os.Exit(1)
+	}
+}
+
+func execute(host, port string) error {
+	mux := http.NewServeMux()
+	bannerSvc := banners.NewService()
+
+	sr := app.NewServer(mux, bannerSvc)
+	sr.Init()
+
+	srv := &http.Server{
+		Addr:    net.JoinHostPort(host, port),
+		Handler: sr,
+	}
+	return srv.ListenAndServe()
+} 
+func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	h.mu.Lock()
+	handler, ok := h.handlers[r.URL.Path]
+	if !ok {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 		return
 	}
+	handler(w, r)
 }
